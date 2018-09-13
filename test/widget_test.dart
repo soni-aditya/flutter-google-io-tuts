300486670c2e7ff76ab5fbccdc12b0df75e367a5
Aditya Soni <adityasoni5859@gmail.com>
2018-09-13 17:29:19 +0530

    feature: written test case for the current flutter app.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:google_flutter_tut/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Clicking a Tap opens it', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(find.byIcon(Icons.launch), findsNothing);
+    await tester.tap(find.byType(ExpansionTile).first);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byIcon(Icons.launch), findsOneWidget);
   });
 }
